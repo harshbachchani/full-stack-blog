4ce38be5fe431177f9a484721d6bb04d23451bd2
harshbachchaniAppPerfect <harshb@AppPerfect.com>
2024-08-26 18:18:57 +0530

    bug fixed

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,1 +1,16 @@
 import { errHandler } from "./middlewares/err.middleware.js";
+import Fastify from "fastify";
+
+const app = Fastify({
+  logger: {
+    transport: {
+      target: "pino-pretty",
+      options: {
+        translateTime: "HH:MM:ss Z",
+        ignore: "pid,hostname",
+      },
+    },
+  },
+});
+
+export default app;
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,18 @@
 import dotenv from "dotenv";
 dotenv.config({ path: "./.env" });
 import connectDB from "./db/index.js";
-
+import app from "./app.js";
 connectDB()
-  .then((value) => {})
+  .then((value) => {
+    app.listen({ port: process.env.PORT }, function (err, address) {
+      if (err) {
+        app.log.error(err);
+        process.exit(1);
+      } else {
+        app.log.info(`Server listening on ${address}`);
+      }
+    });
+  })
   .catch((err) => {
     console.log("MongoDB connection Failed ", err);
   });
