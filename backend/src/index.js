a5a4ea7777becee8781ad5887f7904bd388bd2ce
harshbachchaniAppPerfect <harshb@AppPerfect.com>
2024-08-29 17:23:15 +0530

    updated

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,14 +4,17 @@ import connectDB from "./db/index.js";
 import app from "./app.js";
 connectDB()
   .then(async (value) => {
-    await app.listen({ port: process.env.PORT }, function (err, address) {
-      if (err) {
-        console.log(err);
-        process.exit(1);
-      } else {
-        console.log(`Server listening on ${address}`);
+    await app.listen(
+      { port: process.env.PORT, host: "0.0.0.0" },
+      function (err, address) {
+        if (err) {
+          console.log(err);
+          process.exit(1);
+        } else {
+          console.log(`Server listening on ${address}`);
+        }
       }
-    });
+    );
   })
   .catch((err) => {
     console.log("MongoDB connection Failed ", err);
